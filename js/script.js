313180f69d3c397ad96ff323bca93dd429c6a22e
Luci Peterson <lucijpeterson@gmail.com>
2019-03-27 10:47:30 -0700

    Quote list and functions written
    Added quote array and functions to access random quote within that list and display on page.  Removed unnecessary comments.

--- js/script.js
+++ js/script.js
@@ -1,56 +1,55 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+//LIST OF QUOTES, CREDIT: PINTEREST
+const quotes = [
+  {
+    quote: "You don't need to see the whole staircase, just take the first step.",
+    source: "Martin Luther King, Jr.",
+    citation: "Tiny Positive"
+  },
+  {
+    quote: "Keep your eyes on the stars and your feet on the ground.",
+    source: "Theodore Roosevelt"
+  },
+  {
+    quote: "If you can dream it, you can do it.",
+    source: "Walt Disney"
+  },
+  {
+    quote: "We should all start to live before we get too old.",
+    source: "Marilyn Monroe",
+    year: 1951
+  },
+  {
+    quote: "The best way to predict the future is to create it.",
+    source: "Abraham Lincoln"
+  }
+];
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+//FUNCTIONS
 
+//Chooses a random quote from the list
+function getRandomQuote() {
+  let randomNum = Math.floor(Math.random() * 5);
+  return quotes[randomNum];
+};
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+//Displays random quote on page
+function printQuote() {
+  let randomQuote = getRandomQuote();
+  let htmlString = ''
+  htmlString += "<p class='quote'>" + randomQuote.quote + "</p>" 
+  htmlString += "<p class='source'>" + randomQuote.source
+  if (randomQuote.citation != undefined && randomQuote.year != undefined) {
+    htmlString += "<span class='citation'>" + randomQuote.citation + "</span><span class='year'>" + randomQuote.year + "</span></p>";
+  } else if (randomQuote.citation === undefined && randomQuote.year != undefined) {
+    htmlString += "<span class='year'>" + randomQuote.year + "</span></p>"
+  } else if (randomQuote.citation != undefined && randomQuote.year === undefined) {
+    htmlString += "<span class='citation'>" + randomQuote.citation + "</span></p>"
+  } else {
+    htmlString += "</p>"
+  }
+  const quoteBox = document.getElementById('quote-box');
+    quoteBox.innerHTML = htmlString;
+  };
 
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
+//BUTTON CLICK EVENT USED TO DISPLAY NEXT QUOTE, CREDIT: TEAM TREEHOUSE
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
